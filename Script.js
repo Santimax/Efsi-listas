e7868762b12ab159b0247c2e048e8e393c90b3f2
Your Name <466487878@est.ort.edu.ar>
2023-11-09 21:09:03 -0300

    fin

--- Script.js
+++ Script.js
@@ -1,42 +1,66 @@
-let tareas = [];
+let tasks = [];
 
-const form = document.querySelector('form');
-form.addEventListener('submit', agregarTarea);
-
-function agregarTarea(event) {
-  event.preventDefault();
-  const input = document.querySelector('input');
-  tareas.push({text: input.value, done: false});
-  input.value = '';
-  mostrarTareas();
-}
-
-function tachar(index) {
-  tareas[index].done = !tareas[index].done;
-  mostrarTareas();
-}
-
-function mostrarTareas() {
-  const ul = document.querySelector('ul');
-  ul.innerHTML = '';
-  tareas.forEach((tareas, index) => {
-    const li = document.createElement('li');
-    li.classList.add('list-group-item');
-    const checkbox = document.createElement('input');
-    checkbox.type = 'checkbox';
-    checkbox.checked = tareas.done;
-    checkbox.classList.add('mr-2');
-    checkbox.addEventListener('click', () => tachar(index));
-    li.appendChild(checkbox);
-    const text = document.createElement('span');
-    text.textContent = tareas.text;
-    if (tareas.done) {
-      text.style.textDecoration = 'line-through';
+function agregarTarea() {
+    const tareaInput = document.getElementById("taskInput");
+    const tarea = tareaInput.value.trim();
+    if (tarea !== "") {
+        const timestampInicio = Date.now();
+        const nuevaTarea = {
+            id: timestampInicio,
+            text: tarea,
+            completed: false,
+            timestampInicio: timestampInicio,
+            timestampFin: null,
+        };
+        tasks.push(nuevaTarea);
+        tareaInput.value = "";
+        cargarTareas();
     }
-    li.appendChild(text);
-    ul.appendChild(li);
-  });
 }
 
-const button = document.querySelector('button');
-button.addEventListener('click', mostrarTareas);
+function completarTarea(id) {
+    const tarea = tasks.find(t => t.id === id);
+    if (tarea) {
+        tarea.completed = !tarea.completed;
+        tarea.timestampFin = tarea.completed ? Date.now() : null;
+        cargarTareas();
+    }
+}
+
+function calcularTareaRapida() {
+    const tareaMasRapida = tasks.reduce((tareaAnterior, tareaActual) => {
+        if (tareaAnterior === null) {
+            return tareaActual;
+        }
+
+        const tiempoAnterior = tareaAnterior.timestampFin - tareaAnterior.timestampInicio;
+        const tiempoActual = tareaActual.timestampFin - tareaActual.timestampInicio;
+
+        return tiempoActual < tiempoAnterior ? tareaActual : tareaAnterior;
+    }, null);
+
+    if (tareaMasRapida !== null) {
+        alert(`La tarea más rápida fue: "${tareaMasRapida.text}"`);
+    } else {
+        alert("No hay tareas completadas para calcular la más rápida.");
+    }
+}
+
+function cargarTareas() {
+    const taskList = document.getElementById("taskList");
+    taskList.innerHTML = "";
+    tasks.forEach(tarea => {
+        const listItem = document.createElement("li");
+        listItem.className = `list-group-item ${tarea.completed ? 'list-group-item-success' : ''}`;
+        listItem.innerHTML = `
+            <div class="d-flex justify-content-between align-items-center">
+                <span>${tarea.text}</span>
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" onclick="completarTarea(${tarea.id})"
+                        ${tarea.completed ? 'checked' : ''}>
+                </div>
+            </div>
+        `;
+        taskList.appendChild(listItem);
+    });
+}
